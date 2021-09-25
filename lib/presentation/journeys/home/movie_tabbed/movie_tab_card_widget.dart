8f49a86910da334478972ce7173a46dc8f99ec56
Márcio Quimbundo <marcioquimbundo@hotmail.com>
2021-09-25 22:09:47 +0100

    MovieTabCardWidget added

---
+++ lib/presentation/journeys/home/movie_tabbed/movie_tab_card_widget.dart
@@ -0,0 +1,42 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:movieapp/common/constants/size_constants.dart';
+import 'package:movieapp/data/core/api_constants.dart';
+import 'package:movieapp/common/extensions/size_extensions.dart';
+
+class MovieTabCardWidget extends StatelessWidget {
+  final int movieId;
+  final String title, posterPath;
+  const MovieTabCardWidget(
+      {Key key, @required this.movieId, @required this.title, this.posterPath})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(Sizes.dimen_16.w),
+              child: CachedNetworkImage(
+                  imageUrl: '${ApiConstants.BASE_IMAGE_URL}$posterPath',
+                  fit: BoxFit.cover),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: Sizes.dimen_4.h),
+            child: Text(
+              title,
+              maxLines: 1,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
