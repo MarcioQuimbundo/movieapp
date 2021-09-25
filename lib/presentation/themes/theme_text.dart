117a995c04cf4ccc8a9aa415de0cc096e68e36d3
Márcio Quimbundo <marcioquimbundo@hotmail.com>
2021-09-25 20:51:23 +0100

    textStyle for tabs added

--- lib/presentation/themes/theme_text.dart
+++ lib/presentation/themes/theme_text.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movieapp/common/constants/size_constants.dart';
 import 'package:movieapp/common/extensions/size_extensions.dart';
+import 'package:movieapp/presentation/themes/app_color.dart';
 
 class ThemeText {
   const ThemeText._();
@@ -9,5 +10,27 @@ class ThemeText {
   static TextTheme get _poppinsTextTheme => GoogleFonts.poppinsTextTheme();
   static TextStyle get _whiteHeadline6 => _poppinsTextTheme.headline6
       .copyWith(fontSize: Sizes.dimen_20.sp, color: Colors.white);
-  static getTextTheme() => TextTheme(headline6: _whiteHeadline6);
+
+  static TextStyle get whiteSubtitle1 => _poppinsTextTheme.subtitle1
+      .copyWith(fontSize: Sizes.dimen_16.sp, color: Colors.white);
+
+  static TextStyle get whiteBodyText2 => _poppinsTextTheme.bodyText2.copyWith(
+      fontSize: Sizes.dimen_14.sp,
+      color: Colors.white,
+      wordSpacing: 0.25,
+      letterSpacing: 0.25,
+      height: 1.5);
+
+  static getTextTheme() => TextTheme(
+        headline6: _whiteHeadline6,
+        subtitle1: whiteSubtitle1,
+        bodyText2: whiteBodyText2,
+      );
+}
+
+extension ThemeTextExtension on TextTheme {
+  TextStyle get royalBlueSubtitle1 => subtitle1.copyWith(
+        color: AppColor.royalBlue,
+        fontWeight: FontWeight.w600,
+      );
 }
