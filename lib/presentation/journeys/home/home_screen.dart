0f7639df81e0f65b4e4194a67a1d325b64af3d09
Márcio Quimbundo <marcioquimbundo@hotmail.com>
2021-09-25 22:34:50 +0100

    MovieTabbedWidget added

--- lib/presentation/journeys/home/home_screen.dart
+++ lib/presentation/journeys/home/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:movieapp/di/get_it.dart';
 import 'package:movieapp/presentation/blocs/movie_backdrop/movie_backdrop_bloc.dart';
 import 'package:movieapp/presentation/blocs/movie_carousel/movie_carousel_bloc.dart';
 import 'package:movieapp/presentation/blocs/movie_tabbed/movie_tabbed_bloc.dart';
+import 'package:movieapp/presentation/journeys/home/movie_tabbed/movie_tabbed_widget.dart';
 
 import 'movie_carousel/movie_carousel_widget.dart';
 
@@ -67,7 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   FractionallySizedBox(
                     alignment: Alignment.bottomCenter,
                     heightFactor: 0.4,
-                    child: Placeholder(color: Colors.white),
+                    child: MovieTabbedWidget(),
                   )
                 ],
               );
