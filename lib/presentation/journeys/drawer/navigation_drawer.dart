5c39b377ad642af7ced9ff2f0fd790bbe9855d6d
Márcio Quimbundo <marcioquimbundo@hotmail.com>
2021-09-26 19:01:23 +0100

    NavigationSubListItem, expansion list item added in drawer

--- lib/presentation/journeys/drawer/navigation_drawer.dart
+++ lib/presentation/journeys/drawer/navigation_drawer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:movieapp/common/constants/size_constants.dart';
 import 'package:movieapp/common/extensions/size_extensions.dart';
+import 'package:movieapp/presentation/journeys/drawer/navigation_expanded_list_tile.dart';
+import 'package:movieapp/presentation/journeys/drawer/navigation_list_item.dart';
+import 'package:movieapp/presentation/widgets/logo.dart';
 
 class NavigationDrawer extends StatelessWidget {
   const NavigationDrawer({Key key}) : super(key: key);
@@ -9,7 +12,37 @@ class NavigationDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: Sizes.dimen_300.w,
-      color: Theme.of(context).primaryColor.withOpacity(0.7),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Theme.of(context).primaryColor.withOpacity(0.7),
+            blurRadius: 4,
+          )
+        ],
+      ),
+      child: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+                padding: EdgeInsets.only(
+                    top: Sizes.dimen_8.h,
+                    bottom: Sizes.dimen_18.h,
+                    left: Sizes.dimen_8.w,
+                    right: Sizes.dimen_8.w),
+                child: Logo(
+                  height: Sizes.dimen_20.h,
+                )),
+            NavigationListItem(title: 'Favorite Movies', onPressed: () {}),
+            NavigationExpandedListTile(
+                title: "Language",
+                children: ['English', 'Portuguese'],
+                onPressed: () {}),
+            NavigationListItem(title: 'Feedback', onPressed: () {}),
+            NavigationListItem(title: 'About', onPressed: () {}),
+          ],
+        ),
+      ),
     );
   }
 }
